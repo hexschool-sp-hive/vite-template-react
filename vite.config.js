3d9768a1271c587dc78a898ad3a53a22eecb3388
hokou <hokou.shiau@gmail.com>
2025-02-06 17:00:39 +0800

    fix:  vite config error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENB === 'production' ? 'Repository name' : '/',
-  base: process.env.NODE_ENB === 'production' ? 'vite-template-react' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/Repository name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/vite-template-react/' : '/',
   plugins: [react()],
 })
